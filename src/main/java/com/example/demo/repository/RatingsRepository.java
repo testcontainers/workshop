2ec42a27cb6a5dcee35d595726b1c44dfadca49b
Kevin Wittek <kiview@users.noreply.github.com>
2018-09-11 15:07:28 +0200

    Fix missing constructor in RatingsRepository (#5)

--- src/main/java/com/example/demo/repository/RatingsRepository.java
+++ src/main/java/com/example/demo/repository/RatingsRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
@@ -10,8 +10,11 @@ import java.util.Map;
 @Repository
 public class RatingsRepository {
 
-    @Autowired
     ReactiveRedisOperations<String, String> redisOperations;
+
+    public RatingsRepository(ReactiveRedisTemplate<String, String> stringStringReactiveRedisTemplate) {
+        this.redisOperations = stringStringReactiveRedisTemplate;
+    }
 
     public Mono<Map<Integer, Integer>> findAll(String talkId) {
         return redisOperations.opsForHash()
