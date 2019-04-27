34932a9677b3185bda98ddac2e8388735177684e
Richard North <rich.north@gmail.com>
2019-04-27 17:19:57 +0100

    Tidy imports

--- src/main/java/com/example/demo/api/RatingsController.java
+++ src/main/java/com/example/demo/api/RatingsController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Mono;
 
 import java.util.Map;
 
