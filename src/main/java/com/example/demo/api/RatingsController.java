c98941ab3eb5e42d037ad074023002a891d6c968
Richard North <rich.north@gmail.com>
2019-04-27 17:17:51 +0100

    Convert TalksRepository to blocking

--- src/main/java/com/example/demo/api/RatingsController.java
+++ src/main/java/com/example/demo/api/RatingsController.java
@@ -10,7 +10,6 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
-import java.util.Collections;
 import java.util.Map;
 
 @RestController
@@ -30,16 +29,14 @@ public class RatingsController {
     TalksRepository talksRepository;
 
     @PostMapping
-    public Mono<ResponseEntity<Object>> recordRating(@RequestBody Rating rating) throws Exception {
-        return talksRepository.exists(rating.getTalkId())
-                .filter(Boolean.TRUE::equals)
-                .flatMap(__ -> Mono
-                        .fromCompletionStage(
-                                kafkaTemplate.send("ratings", rating).completable()
-                        )
-                        .thenReturn(ResponseEntity.accepted().build())
-                )
-                .defaultIfEmpty(ResponseEntity.badRequest().build());
+    public ResponseEntity<Object> recordRating(@RequestBody Rating rating) throws Exception {
+
+        if (!talksRepository.exists(rating.getTalkId())) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        kafkaTemplate.send("ratings", rating).get();
+        return ResponseEntity.accepted().build();
     }
 
     @GetMapping
--- src/main/java/com/example/demo/repository/TalksRepository.java
+++ src/main/java/com/example/demo/repository/TalksRepository.java
@@ -3,8 +3,6 @@ package com.example.demo.repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 import java.util.List;
 
@@ -14,16 +12,12 @@ public class TalksRepository {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    public Mono<Boolean> exists(String talkId) {
-        return Mono
-                .fromCallable(() -> {
-                    List<Boolean> results = jdbcTemplate.query(
-                            "SELECT 1 FROM talks WHERE id = ?",
-                            (row, i) -> true,
-                            talkId
-                    );
-                    return !results.isEmpty();
-                })
-                .subscribeOn(Schedulers.elastic());
+    public Boolean exists(String talkId) {
+        List<Boolean> results = jdbcTemplate.query(
+                "SELECT 1 FROM talks WHERE id = ?",
+                (row, i) -> true,
+                talkId
+        );
+        return !results.isEmpty();
     }
 }
