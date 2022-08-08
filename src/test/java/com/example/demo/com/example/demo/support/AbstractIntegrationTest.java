4f511627238c280a89d845459c2adeced400e724
shelajev <oleg@atomicjar.com>
2022-08-08 22:20:49 +0300

    this is the beginning

--- src/test/java/com/example/demo/com/example/demo/support/AbstractIntegrationTest.java
+++ src/test/java/com/example/demo/com/example/demo/support/AbstractIntegrationTest.java
@@ -37,57 +37,6 @@ import java.io.IOException;
 )
 public class AbstractIntegrationTest {
 
-  static Network network = Network.newNetwork();
-
-  static GenericContainer<?> redis = new GenericContainer<>("redis:6-alpine")
-    .withExposedPorts(6379).withNetwork(network).withNetworkAliases("redis");
-
-  static PostgreSQLContainer<?> postgreSQLContainer =
-    new PostgreSQLContainer<>("postgres:14-alpine");
-
-  static KafkaContainer kafka = new KafkaContainer(
-    DockerImageName.parse("confluentinc/cp-kafka:5.4.6"));
-
-  static ToxiproxyContainer toxyproxy = new ToxiproxyContainer(
-    DockerImageName.parse("shopify/toxiproxy:2.1.0")).withNetwork(network);
-
-  @DynamicPropertySource
-  public static void setupThings(DynamicPropertyRegistry registry) throws IOException {
-    Startables.deepStart(redis, kafka, postgreSQLContainer, toxyproxy).join();
-
-    ToxiproxyContainer.ContainerProxy proxy = toxyproxy.getProxy(redis, 6379);
-    proxy.toxics().latency("latency", ToxicDirection.DOWNSTREAM, 2000).setJitter(200);
-
-    registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-    registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-    registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-
-    registry.add("spring.redis.host", proxy::getContainerIpAddress);
-    registry.add("spring.redis.port", proxy::getProxyPort);
-
-    registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
-
-    createProxy();
-  }
-
-  static  TcpProxy tcpProxy;
-
-  public static void createProxy() {
-    StaticTcpProxyConfig config = new StaticTcpProxyConfig(
-      5900,
-      postgreSQLContainer.getHost(),
-      postgreSQLContainer.getFirstMappedPort()
-    );
-    config.setWorkerCount(1);
-    tcpProxy = new TcpProxy(config);
-    tcpProxy.start();
-  }
-
-  @AfterAll
-  public static void stopProxy() {
-    tcpProxy.shutdown();
-  }
-
   protected RequestSpecification requestSpecification;
 
   @LocalServerPort
